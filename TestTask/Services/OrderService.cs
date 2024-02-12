29a8373f750e5c663ef151bcc891634a93156ee2
egor chuzhavko <egorchuzhavko@gmail.com>
2024-02-12 18:07:11 +0300

    Add project files.

---
+++ TestTask/Services/OrderService.cs
@@ -0,0 +1,26 @@
+﻿using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services {
+    public class OrderService : IOrderService {
+        private readonly ApplicationDbContext _context;
+
+        public OrderService(ApplicationDbContext context) {
+            _context = context;
+        }
+        public Task<Order> GetOrder() {
+            var order = _context.Orders
+                .OrderByDescending(o => o.Price * o.Quantity)
+                .FirstOrDefault();
+            return Task.FromResult(order);
+        }
+
+        public Task<List<Order>> GetOrders() {
+            var orders = _context.Orders
+                .Where(o => o.Quantity > 10)
+                .ToList();
+            return Task.FromResult(orders);
+        }
+    }
+}
---
+++ TestTask/Services/UserService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services {
+    public class UserService : IUserService {
+        private readonly ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public Task<User> GetUser() {
+            var result = _context.Users
+                .OrderByDescending(u => u.Orders.Count)
+                .FirstOrDefault();
+            return Task.FromResult(result);
+        }
+
+        public Task<List<User>> GetUsers() {
+            var users = _context.Users.Where(u => u.Status == Enums.UserStatus.Inactive).ToList();
+            return Task.FromResult(users);
+        }
+    }
+}
